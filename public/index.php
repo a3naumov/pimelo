3d3e49af7ab1097b575a72a4b4a5e93517654d75
Artem Naumov <naumovartem272@gmail.com>
2025-12-28 12:58:48 +0400

    Refactor project structure to move bootstrap files to a dedicated directory

--- public/index.php
+++ public/index.php
@@ -2,8 +2,8 @@
 
 declare(strict_types=1);
 
-require_once dirname(__DIR__).'/app/autoload_runtime.php';
+require_once dirname(__DIR__).'/bootstrap/autoload_runtime.php';
 
-$kernel = require_once dirname(__DIR__).'/app/kernel.php';
+$kernel = require_once dirname(__DIR__).'/bootstrap/kernel.php';
 
 return static fn (array $context) => $kernel($context);
